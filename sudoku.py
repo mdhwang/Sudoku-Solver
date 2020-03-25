dc25eea9b2eb58dd75a5a45f4e1932cce93fd2f7
mdhwang <mattdhwang@gmail.com>
2020-03-24 19:45:30 -0700

    first pass solution

--- sudoku.py
+++ sudoku.py
@@ -1,13 +1,13 @@
 board = [
-    [0,1,0,0,8,6,0,0,7],
-    [0,0,0,0,0,4,9,0,5],
-    [0,0,0,1,0,0,0,0,0],
-    [0,7,0,0,5,0,0,3,2],
-    [0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,9,0,7,0,0],
-    [0,0,6,0,1,0,0,0,9],
-    [3,4,0,0,0,0,0,0,1],
-    [8,0,0,0,3,0,0,5,0]
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
 ]
 
 
@@ -24,7 +24,7 @@ def format(board):
             else:
                 print(str(board[i][j]) + " ", end="")
     print("")
-format(board)
+
 
 def find_empty(board):
     for i in range(len(board)):
@@ -32,3 +32,43 @@ def find_empty(board):
             if board[i][j] == 0:
                 return (i,j)
     return None
+
+def validate(board, number, position):
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False        
+
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if board[i][j] == number and (i,j) != position:
+                return False
+
+    return True
+
+def solve(board):
+    empty = find_empty(board)
+    if not empty:
+        return True
+    else:
+        row, col = empty
+    
+    for i in range(1,10):
+        if validate(board, i, (row,col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+            
+            board[row][col] = 0
+    return False
+
+format(board)
+solve(board)
+format(board)
